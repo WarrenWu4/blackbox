33fca2e0731d21435a0638c81b64a3430a623afb
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-03 03:14:10 -0500

    frontend dashboard edits

--- src/components/blackbox/blackbox.js
+++ src/components/blackbox/blackbox.js
@@ -1,219 +1,127 @@
-import React from "react"
-import { RxCube } from "react-icons/rx";
-import { FaFileUpload, FaUpload } from "react-icons/fa";
-import { ImArrowRight } from "react-icons/im";
-import { useState, useRef, useEffect } from "react";
-import { storage, db } from "../../firebase"
-import { ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
-import { collection, addDoc, getDocs, getDoc, doc, deleteDoc } from "firebase/firestore";
-import File from "./file";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import { Modal } from "@mui/material";
+import { db } from "../../firebase";
+import { getDocs, collection } from "firebase/firestore";
+
+import { BsThreeDotsVertical, BsFileEarmarkPlay } from "react-icons/bs";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { HiOutlineHome, HiSortAscending } from "react-icons/hi";
 
 export default function Blackbox() {
 
-    const nav = useNavigate()
-    const [admin, setAdmin] = useState(false);
+    const [isLoadingData, setIsLoadingData] = useState(true);
+    const [data, setData] = useState()
 
     useEffect(() => {
 
-        const getToken = async () => {
-
-            // todo display loading screen while authenticating
-
-            const token = localStorage.getItem("blackbox_id");
-            if (token === null) {
-                nav("/");
-            }
-            else {
-                const user = await getDoc(doc(db, "users", token))
-                if (!(user.exists())) {
-                    nav("/");
-                }
-                else {
-                    // set user perms
-                    setAdmin(user.data().admin)
-                }
-            }
+        const getData = async () => {
+            const querySnapshot = await getDocs(collection(db, "data"));
+            setData(querySnapshot);
+            setIsLoadingData(false);
         }
 
-        getToken();
+        getData()
 
     }, [])
 
-    // references to get input values
-    const urlRef = useRef();
-    const nameRef = useRef();
+    return (
+        <>
+            {!isLoadingData && <div className="w-screen min-h-screen flex flex-col items-center px-[3.2rem] py-[4rem] bg-black overflow-x-hidden">
+                
+                {data.docs.map((doc) =>
+                    <FileCard key={doc.id} name={doc.data()["name"]} fileType={doc.data()["type"]} source={doc.data()["source"]}/>
+                )}
 
-    // store modal display state for upload
-    const [addFile, setAddFile] = useState(false);
-    // store file on change for input
-    const [inFile, setInFile] = useState("")
-    // store file components
-    const [uploads, setUploads] = useState([])
-    // store the id of component that's going to be deleted
-    const [delId, setDelId] = useState("");
-    // store modal display state for delete
-    const [addDel, setAddDel] = useState(false);
+                <Controls/>
 
-    // takes in file name, link, and type and stores it in firestore as well as storage if from "uploads" section
-    const uploadData = async (e) => {
-        e.preventDefault()
-        // uploading requires admin perms
-        if (admin) {
-            // if there's a url --> store in firestore
-            if(urlRef.current.value !== "" && nameRef.current.value !== "") {
-                console.log("UPLOADING LINKS......\n")
-                try {
-                    await addDoc(collection(db, "data"), {
-                        link: urlRef.current.value,
-                        name: nameRef.current.value,
-                        type: "link"
-                    })
-                } catch(err) {
-                    console.log("LINK UPLOAD ERROR:\n", err)
-                }
-            }
-            // if there's a file upload --> store in storage then store in firestore
-            if(inFile !== "") {
-                console.log("UPLOADING FILES......")
-                await uploadBytesResumable(ref(storage, inFile.name), inFile);
-                await getDownloadURL(ref(storage, inFile.name)).then((url) => {
-                    try {
-                        addDoc(collection(db, "data"), {
-                            link: url,
-                            name: inFile.name,
-                            type: "file"
-                        })
-                    } catch(err) {
-                        console.log("FILE UPLOAD ERROR:\n", err)
-                    }
-                })
-            }
-        }
-        // reset parameters
-        setInFile("");
-        urlRef.current.value = "";
-        nameRef.current.value = "";
-        setAddFile(false);
+            </div>}
+        </>
+    )
+}
 
-    };
+const FileCard = ({name, fileType, source}) => {
 
-    // takes in an id and deletes it from firestore as well as storage if from "uploads" section
-    const deleteData = async () => {
-        //requires admin perms
-        if (admin) {
-            // get data from id
-            const docu = await getDoc(doc(db, "data", delId))
-            const type = docu.data().type;
-            // if upload type is file --> delete from storage and firestore
-            if (type === "file") { 
-                console.log("DELETING FILE.....")
-                // deleting from storage
-                deleteObject(ref(storage, docu.data().name)).then(() => {
-                    console.log("File deleted successfully")
-                }).catch((err) => {
-                    console.log("DELETING FILE ERROR:\n", err)
-                })
-
-                // delete from firestore
-                await deleteDoc(doc(db, "data", delId));
-            } 
-            // otherwise just delete the upload link type from firestore
-            else {
-                console.log("DELETING LINK....")
-                await deleteDoc(doc(db, "data", delId))
-            }
-
-            // close modal at end
-            setAddDel(false);
-        }
+    const handleClick = () => {
+        console.log("WIP")
     }
-
-    // fetches and displays data
-    const readData = async () => {
-        const data = await getDocs(collection(db, "data"));
-        const fileComp = data.docs.map((doc) => {
-            return <File url={doc.data().link} name={doc.data().name} id={doc.id} setId={setDelId} />
-        })
-        setUploads(fileComp);
-    }
-
-    // rerenders data on upload and delId
-    useEffect(() => {
-
-        readData()
-    
-    }, [addFile, addDel])
-
-    //deletes data on delID change
-    useEffect(() => {
-
-        if (delId !== "") {
-            setAddDel(true)
-        }
-
-    }, [delId])
 
     return (
-        <div className="blackbox">
-            <div className="blackbox-row">
-                <div className="blackbox-logo">
-                    <RxCube/>
-                    blackbox
-                </div>
-                <div className="blackbox-about">about</div>
-            </div>
-            <div className="blackbox-info">
-                <div className="blackbox-path">
-                    Root/
-                </div>
-                {admin &&
-                    <div className="blackbox-files" onClick={() => setAddFile(true)}>
-                        <FaFileUpload/>
-                    </div>
-                }
-            </div>
-            <div className="blackbox-divider"></div>
+        <a href={source} target="_blank" className="max-w-[64rem] w-full h-[5.6rem] backdrop-blur-[2rem] rounded-[0.8rem] bg-glass flex justify-between items-center text-white px-[2.4rem] mb-[1.6rem]">
 
-            <div className="blackbox-content">
-                {uploads}
+            <div className="text-[2rem] font-semibold flex items-center">
+                <BsFileEarmarkPlay className="text-[2rem] mr-[0.8rem]"/>
+                <span className="flex text-[2rem] overflow-hidden text-ellipsis whitespace-nowrap">{name}</span>
             </div>
 
-            {/* uploads form */}
-            <div className="blackbox-upload-file" style={{display:(addFile)?"flex":"none"}}>
+            <button onClick={handleClick} type="button">
+                <BsThreeDotsVertical className="text-[2rem]"/>
+            </button>
 
-                <form className="blackbox-form" onSubmit={uploadData}>
+        </a>
+    )
+}
 
-                    <label className="blackbox-label">Upload File / URL</label>
-                    <input type="text" className="blackbox-input-url" ref={nameRef} placeholder="name"></input>
-                    <input type="url" className="blackbox-input-url" ref={urlRef} placeholder="link"></input>
+const Controls = () => {
 
-                    <div style={{display: "flex"}}>
+    const [modal, setModal] = useState(false);
 
-                        <label className="blackbox-input-file">
-                            <FaUpload/>
-                            <input type="file" onChange={(e) => setInFile(e.target.files[0])}></input>
-                        </label>
+    const scrollTop = () => {
+        window.scrollTo({top:0, behavior: 'smooth'})
+    }
 
-                        <button className='blackbox-upload-btn' type='submit'>
-                            <ImArrowRight/>
-                        </button>
+    const sortFiles = () => {
+        console.log("WIP")
+    }
 
-                    </div>
+    const addFile = () => {
+        console.log("WIP")
+        // --> carry out the operation on the backend -> reflect changes on frontend manually for better speeds
+    }
 
-                </form>
+    return (
+        <>
+        <div className="fixed bottom-0 h-[13.6rem] w-full bg-black flex items-center justify-center">
 
-            </div>
+            <div className="fixed bottom-[4rem] bg-glass flex items-center justify-evenly text-white w-[19.2rem] h-[5.6rem] rounded-[0.8rem] backdrop-blur-[2rem]">
 
-            {/* confirm delete */}
-            <div className="blackbox-upload-file" style={{display:(addDel)?"flex":"none"}} onContextMenu={(e) => e.preventDefault()}>
-                Are you sure you want to delete?
-                <div className="blackbox-del-confirm">
-                    <button className="blackbox-del-btn" onClick={() => setAddDel(false)}>NO!</button>
-                    <button className="blackbox-del-btn" onClick={deleteData}>YES</button>
-                </div>
+                <button onClick={scrollTop} type="button"><HiOutlineHome className="text-[3.2rem]"/></button>
+
+                <button onClick={() => setModal(true)} type="button"><IoMdAddCircleOutline className="text-[3.2rem]"/></button>
+
+                <button onClick={sortFiles} type="button"><HiSortAscending className="text-[3.2rem]"/></button>
+
             </div>
 
         </div>
+
+        <Modal
+            open={modal}
+            onClose={() => setModal(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+                <form onSubmit={addFile} className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-[1.6rem] bg-glass flex text-white rounded-[0.8rem] backdrop-blur-[2rem] flex-col">
+                
+                    <label className="text-[2rem] font-bold">Name</label>
+                    <input type="text" className="rounded-[0.4rem] bg-white mt-[0.4rem] text-[1.6rem] text-black px-[0.8rem] py-[0.4rem]" required/>
+
+                    <label className="text-[2rem] font-bold mt-[1.6rem]">File Type</label>
+                    <select className="text-[1.6rem] px-[0.8rem] py-[0.4rem] text-black bg-white rounded-[0.4rem] mt-[0.4rem]" required>
+                        <option value="pdf">PDF</option>
+                        <option value="doc">DOC</option>
+                        <option value="vid">VID</option>
+                    </select>
+
+                    <label className="text-[2rem] font-bold mt-[1.6rem]">Source</label>
+                    <div className="flex items-center justify-center mt-[0.4rem]">
+                        <input type="text" className="mr-[0.8rem] text-[1.6rem] w-3/4 px-[0.8rem] py-[0.4rem] rounded-[0.4rem] bg-white text-black" />
+                        <input type="file" className="ml-[0.8rem] text-[1.6rem] w-1/4 px-[0.8rem] py-[0.4rem] rounded-[0.4rem] bg-white text-black appearance-none" />
+                    </div>
+
+                    <button type="submit" className="w-full rounded-[0.4rem] bg-black text-white font-bold text-[1.6rem] py-[0.8rem] mt-[2.8rem]">SUBMIT</button>
+
+                </form>
+            </Modal>
+        </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/index.html","./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'glass': "linear-gradient(135deg, #FFF 0%, rgba(255, 255, 255, 0.40) 0.01%, rgba(255, 255, 255, 0.25) 100%)",
+      },
+    },
   },
   plugins: [],
 }
