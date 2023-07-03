b9b117a124eb01fa0828be67b20e9ab7e429d37d
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-03 12:36:01 -0500

    massive updates to blackbox, rerenders on data change, add popover for menu, added delete and create file functions for admin

--- src/components/blackbox.js
+++ src/components/blackbox.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react"
-import { Modal } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react"
+import { Modal, Popover } from "@mui/material";
 import { db } from "../firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, deleteDoc, doc, Timestamp, addDoc } from "firebase/firestore";
 
 import { BsThreeDotsVertical, BsFileEarmarkPlay } from "react-icons/bs";
 import { IoMdAddCircleOutline } from "react-icons/io";
@@ -22,16 +22,16 @@ export default function Blackbox({role, name}) {
 
         getData()
 
-    }, [])
+    }, [data])
 
     return (
         <>
             {!isLoadingData && <div className="w-screen min-h-screen flex flex-col items-center px-[3.2rem] py-[4rem] bg-black overflow-x-hidden">
                 
-                <span className="text-white text-[2.4rem] font-bold flex items-center mb-[4rem]">Hello&nbsp;<span className="text-[#4c9671]">{name}</span> <img src="./default.png" alt="default_logo" className="w-[4.8rem] aspect-square ml-[1.2rem] rounded-[0.8rem] bg-glass p-[0.8rem]"/></span>
+                <span className="text-white text-[2.4rem] font-bold flex items-center mb-[4rem]">Hello&nbsp;<span className="text-[#4c9671] text-center">{name}</span> <img src="./default.png" alt="default_logo" className="w-[4.8rem] aspect-square ml-[1.2rem] rounded-[0.8rem] bg-glass p-[0.8rem]"/></span>
 
                 {data.docs.map((doc) =>
-                    <FileCard key={doc.id} name={doc.data()["name"]} fileType={doc.data()["type"]} source={doc.data()["source"]}/>
+                    <FileCard key={doc.id} docId={doc.id} name={doc.data()["name"]} fileType={doc.data()["type"]} source={doc.data()["source"]}/>
                 )}
 
                 {(role === "admin") && <Controls/>}
@@ -41,30 +41,68 @@ export default function Blackbox({role, name}) {
     )
 }
 
-const FileCard = ({name, fileType, source}) => {
+const FileCard = ({name, fileType, source, docId}) => {
 
-    const handleClick = () => {
+    const [menu, setMenu] = useState(false);
+    const btnRef = useRef()
+    const editItem = (e) => {
+        e.preventDefault()
         console.log("WIP")
+    }
+    const deleteItem = async (e) => {
+        e.preventDefault()
+        await deleteDoc(doc(db, "data", docId))
+        setMenu(false);
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setMenu(true);
     }
 
     return (
-        <a href={source} target="_blank" className="max-w-[64rem] w-full h-[5.6rem] backdrop-blur-[2rem] rounded-[0.8rem] bg-glass flex justify-between items-center text-white px-[2.4rem] mb-[1.6rem]">
+        <>
+            <a href={source} target="_blank" className="max-w-[64rem] w-full h-[5.6rem] backdrop-blur-[2rem] rounded-[0.8rem] bg-glass flex justify-between items-center text-white px-[1.6rem] mb-[1.6rem]">
 
-            <div className="text-[1.6rem] flex items-center">
-                <BsFileEarmarkPlay className="text-[2rem] mr-[0.8rem]"/>
-                <span className="flex overflow-hidden text-ellipsis whitespace-nowrap">{name}</span>
-            </div>
+                <div className="text-[2rem] flex items-center">
+                    <BsFileEarmarkPlay className="text-[2.4rem] mr-[0.8rem]"/>
+                    <span className="flex overflow-hidden text-ellipsis whitespace-nowrap">{name}</span>
+                </div>
 
-            <button onClick={handleClick} type="button">
-                <BsThreeDotsVertical className="text-[2rem]"/>
-            </button>
+                <button onClick={handleClick} ref={btnRef} type="button">
+                    <BsThreeDotsVertical className="text-[2.4rem]"/>
+                </button>
+                <Popover
+                    className="mt-[0.8rem]"
+                    open={menu}
+                    anchorEl={btnRef.current}
+                    onClose={() => setMenu(false)}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                >
+                    <div className="flex flex-col text-[1.2rem] text-black">
+                        <button onClick={editItem} type="button" className="px-[1.6rem] py-[0.8rem] hover:bg-black/20 transition-all duration-[0.6s]">Edit</button>
+                        <button onClick={deleteItem} type="button" className="px-[1.6rem] py-[0.8rem] hover:bg-black/20 transition-all duration-[0.6s]">Delete</button>
+                    </div>
+                </Popover>
 
-        </a>
+            </a>
+
+        </>
     )
 }
 
 const Controls = () => {
 
+    const fileName = useRef()
+    const fileType = useRef()
+    const fileSource = useRef()
     const [modal, setModal] = useState(false);
 
     const scrollTop = () => {
@@ -75,22 +113,36 @@ const Controls = () => {
         console.log("WIP")
     }
 
-    const addFile = () => {
-        console.log("WIP")
-        // --> carry out the operation on the backend -> reflect changes on frontend manually for better speeds
+    const addFile = async (e) => {
+        e.preventDefault()
+        const fName = fileName.current.value
+        const fType = fileType.current.value
+        const fSource = fileSource.current.value
+
+        await addDoc(collection(db, "data"), {
+            name: fName,
+            source: fSource,
+            type: fType,
+            created: Timestamp.now(),
+        })
+
+        fileName.current.value = ""
+        fileType.current.value = "" 
+        fileSource.current.value = ""
+        setModal(false)
     }
 
     return (
         <>
         <div className="fixed bottom-0 h-[13.6rem] w-full bg-black flex items-center justify-center">
 
-            <div className="fixed bottom-[4rem] bg-glass flex items-center justify-evenly text-white w-[19.2rem] h-[5.6rem] rounded-[0.8rem] backdrop-blur-[2rem]">
+            <div className="fixed bottom-[4rem] bg-glass flex items-center justify-evenly text-white w-[16rem] h-[5.6rem] rounded-[0.8rem] backdrop-blur-[2rem]">
 
-                <button onClick={scrollTop} type="button"><HiOutlineHome className="text-[3.2rem]"/></button>
+                <button onClick={scrollTop} type="button"><HiOutlineHome className="text-[2.4rem]"/></button>
 
-                <button onClick={() => setModal(true)} type="button"><IoMdAddCircleOutline className="text-[3.2rem]"/></button>
+                <button onClick={() => setModal(true)} type="button"><IoMdAddCircleOutline className="text-[2.4rem]"/></button>
 
-                <button onClick={sortFiles} type="button"><HiSortAscending className="text-[3.2rem]"/></button>
+                <button onClick={sortFiles} type="button"><HiSortAscending className="text-[2.4rem]"/></button>
 
             </div>
 
@@ -105,10 +157,10 @@ const Controls = () => {
                 <form onSubmit={addFile} className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-[1.6rem] bg-glass flex text-white rounded-[0.8rem] backdrop-blur-[2rem] flex-col">
                 
                     <label className="text-[2rem] font-bold">Name</label>
-                    <input type="text" className="rounded-[0.4rem] bg-white mt-[0.4rem] text-[1.6rem] text-black px-[0.8rem] py-[0.4rem]" required/>
+                    <input ref={fileName} type="text" className="rounded-[0.4rem] bg-white mt-[0.4rem] text-[1.6rem] text-black px-[0.8rem] py-[0.4rem]" required/>
 
                     <label className="text-[2rem] font-bold mt-[1.6rem]">File Type</label>
-                    <select className="text-[1.6rem] px-[0.8rem] py-[0.4rem] text-black bg-white rounded-[0.4rem] mt-[0.4rem]" required>
+                    <select ref={fileType} className="text-[1.6rem] px-[0.8rem] py-[0.4rem] text-black bg-white rounded-[0.4rem] mt-[0.4rem]" required>
                         <option value="pdf">PDF</option>
                         <option value="doc">DOC</option>
                         <option value="vid">VID</option>
@@ -116,7 +168,7 @@ const Controls = () => {
 
                     <label className="text-[2rem] font-bold mt-[1.6rem]">Source</label>
                     <div className="flex items-center justify-center mt-[0.4rem]">
-                        <input type="text" className="mr-[0.8rem] text-[1.6rem] w-3/4 px-[0.8rem] py-[0.4rem] rounded-[0.4rem] bg-white text-black" />
+                        <input ref={fileSource} type="text" className="mr-[0.8rem] text-[1.6rem] w-3/4 px-[0.8rem] py-[0.4rem] rounded-[0.4rem] bg-white text-black" />
                         <input type="file" className="ml-[0.8rem] text-[1.6rem] w-1/4 px-[0.8rem] py-[0.4rem] rounded-[0.4rem] bg-white text-black appearance-none" />
                     </div>
 
